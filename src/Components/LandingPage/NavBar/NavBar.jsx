cd916f8215a9cac4f39c18eb5b8152e4986d78d1
Dandarawy <abdullah.dandarawy@gmail.com>
2026-03-08 16:30:58 +0200

    fixed navbar

--- src/Components/LandingPage/NavBar/NavBar.jsx
+++ src/Components/LandingPage/NavBar/NavBar.jsx
@@ -26,7 +26,7 @@ function NavBar() {
         <span><a href="/en"> EN </a></span>
         <span> | </span>
         <span><a href="/ar"> AR </a></span>
-      </div>
+      </div >
         <button className='login-btn'>Login</button>
         <button className='signup-btn'>Sign Up</button>
       </div>
